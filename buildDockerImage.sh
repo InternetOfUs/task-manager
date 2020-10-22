07081e0eb04aa13ca610e3a4b3e21310c606f9ea
Bruno Rosell i Gui (UDT-IA, IIIA-CSIC) <rosell@localhost>
2020-10-22 15:44:16 +0200

    Update yo the new docker container names.

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR >/dev/null
 COMPONENT_VERSION=$(grep --max-count=1 '<version>' pom.xml | awk -F '>' '{ print $2 }' | awk -F '<' '{ print $1 }')
-COMPONENT_NAME="wenet/task-manager"
+COMPONENT_NAME="internetofus/task-manager"
 DOCKER_TAG="$COMPONENT_NAME:$COMPONENT_VERSION"
 DOCKER_BUILDKIT=1 docker build -f src/main/docker/Dockerfile -t $DOCKER_TAG .
 popd >/dev/null
