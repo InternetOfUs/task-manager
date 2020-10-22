211a12ae39fd1cf93ceafb492cb6b73aa7454867
Bruno Rosell i Gui (UDT-IA, IIIA-CSIC) <rosell@localhost>
2020-10-22 17:51:13 +0200

    Rename docker image.

--- startDevelopmentEnvironment.sh
+++ startDevelopmentEnvironment.sh
@@ -2,6 +2,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR >/dev/null
 docker-compose -p wenet_task_manager_services_dev -f src/dev/docker/docker-compose.yml up --remove-orphans -d
-DOCKER_BUILDKIT=1 docker build -f src/dev/docker/Dockerfile -t wenet/task-manager:dev .
-docker run --name wenet_task_manager_dev -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/.m2/repository:/root/.m2/repository  -v ${PWD}:/app -it wenet/task-manager:dev /bin/bash
+DOCKER_BUILDKIT=1 docker build -f src/dev/docker/Dockerfile -t internetofus/task-manager:dev .
+docker run --name wenet_task_manager_dev -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/.m2/repository:/root/.m2/repository  -v ${PWD}:/app -it internetofus/task-manager:dev /bin/bash
 popd >/dev/null
