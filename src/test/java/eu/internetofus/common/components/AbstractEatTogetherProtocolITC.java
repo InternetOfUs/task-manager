0dca4bab723e45d999e939e25106fab6c9eb9130
Bruno Rosell i Gui <rosell@iiia.csic.es>
2021-03-02 18:42:00 +0100

    Add create task norms

--- src/test/java/eu/internetofus/common/components/AbstractEatTogetherProtocolITC.java
+++ src/test/java/eu/internetofus/common/components/AbstractEatTogetherProtocolITC.java
@@ -557,7 +557,8 @@ public abstract class AbstractEatTogetherProtocolITC extends AbstractProtocolITC
   @Order(22)
   public void shouldNotCreateTaskWithWithoutDeadline(final Vertx vertx, final VertxTestContext testContext) {
 
-    this.assertLastSuccessfulTestWas(21, testContext);
+    this.assertAtLeastSuccessfulTestWas(4, testContext);
+    this.lastSuccessfulTest = 21;
 
     final var task = this.createTaskForProtocol();
     task.attributes.remove("deadlineTs");
@@ -582,7 +583,8 @@ public abstract class AbstractEatTogetherProtocolITC extends AbstractProtocolITC
   @Order(23)
   public void shouldNotCreateTaskWithWithBadDeadline(final Vertx vertx, final VertxTestContext testContext) {
 
-    this.assertLastSuccessfulTestWas(22, testContext);
+    this.assertAtLeastSuccessfulTestWas(4, testContext);
+    this.lastSuccessfulTest = 22;
 
     final var task = this.createTaskForProtocol();
     task.attributes.put("deadlineTs", TimeManager.now());
@@ -607,7 +609,8 @@ public abstract class AbstractEatTogetherProtocolITC extends AbstractProtocolITC
   @Order(24)
   public void shouldCreateTaskWithShortDeadline(final Vertx vertx, final VertxTestContext testContext) {
 
-    this.assertLastSuccessfulTestWas(23, testContext);
+    this.assertAtLeastSuccessfulTestWas(4, testContext);
+    this.lastSuccessfulTest = 23;
 
     final var source = this.createTaskForProtocol();
     source.attributes.put("deadlineTs", TimeManager.now() + 10);
