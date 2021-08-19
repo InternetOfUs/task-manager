8be9347e5b108c82589e0b449635e824c749dc34
Bruno Rosell i Gui <rosell@iiia.csic.es>
2021-08-19 14:27:54 +0200

    Auto stop environment when exit

--- stopDevelopmentEnvironment.sh
+++ stopDevelopmentEnvironment.sh
@@ -5,7 +5,9 @@ else
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 	pushd $DIR >/dev/null
 	docker-compose -p wenet_task_manager_services_dev -f src/dev/docker/docker-compose.yml down --remove-orphans
-	docker stop wenet_task_manager_dev
-	docker rm wenet_task_manager_dev
+	if [ "$(docker container ls |grep wenet_task_manager_dev |wc -l)" -gt "0" ]
+	then
+		docker stop wenet_task_manager_dev
+	fi
 	popd >/dev/null
 fi
