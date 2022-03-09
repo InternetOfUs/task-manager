6924b342f52dea8b434ae886e0413d0c8da2c96a
Bruno Rosell i Gui <rosell@iiia.csic.es>
2022-03-09 13:37:52 +0100

    Solve testcontainers error on mac

--- startDevelopmentEnvironment.sh
+++ startDevelopmentEnvironment.sh
@@ -5,7 +5,13 @@ else
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 	pushd "$DIR" >/dev/null
 	DOCKER_BUILDKIT=1 docker build -f src/dev/docker/Dockerfile -t internetofus/task-manager:dev .
-	docker run --rm --name wenet_task_manager_dev -v /var/run/docker.sock:/var/run/docker.sock -v "${HOME}/.m2/repository":/root/.m2/repository  -v "${PWD}":/app -p 5002:5005 -it internetofus/task-manager:dev /bin/bash
-	./stopDevelopmentEnvironment.sh
+		if [ $? -eq 0 ]; then
+		DOCKER_PARAMS="--rm --name wenet_task_manager_dev -v /var/run/docker.sock:/var/run/docker.sock -p 5003:5005 -it"
+		if [[ "$OSTYPE" == "darwin"* ]]; then
+			DOCKER_PARAMS="$DOCKER_PARAMS -e TESTCONTAINERS_HOST_OVERRIDE=docker.for.mac.host.internal"
+		fi
+		docker run $DOCKER_PARAMS -v "${HOME}/.m2/repository":/root/.m2/repository  -v "${PWD}":/app internetofus/task-manager:dev /bin/bash
+		./stopDevelopmentEnvironment.sh
+	fi
 	popd >/dev/null
 fi
